951319aa8a6bdb8f2090c562d94d42925be79982
Diderik Kramer <diderikk@stud.ntnu.no>
2021-03-13 15:49:32 +0100

    Oving 4

---
+++ Oving4/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <h2 v-if="showAnswer">{{ answer }}</h2>
+    <h2 v-else-if="input.length">{{ currentCalc }}</h2>
+    <h2 v-else>0</h2>
+    <Keyboard @keyClick="handleKeyClick" />
+    <Log :calculations="calculations"/>
+  </main>
+</template>
+
+<script>
+import Keyboard from "./components/Keyboard"
+import Log from "./components/Log"
+
+export default {
+  name: 'App',
+  components: { Keyboard, Log },
+  data(){
+    return {
+      title: "Calculator",
+      image: "./assets/calculator.jpg",
+      input: [],
+      answer: '0',
+      showAnswer: false,
+      calculations: []
+    }
+  },
+  methods: {
+    handleKeyClick(key){
+      this.showAnswer = false;
+      if((key === '*' || key === '/' || key === '+' || key === '-') && this.answer != '0' && this.input.length === 0){
+        this.input.push('ANS', key);
+        return;
+      }
+      switch(key){
+        case 'DEL':
+          this.input.pop();
+          break;
+        case 'C':
+          this.input = [];
+          break;
+        case '=':
+          this.answer = eval(this.getFullCalculation);
+          if(this.answer % 1 === 0) this.answer = this.answer.toString();
+          else this.answer = this.answer.toFixed(3);
+          this.showAnswer = true;
+          console.log(this.answer);
+
+          this.input.push(" ", key," ", this.answer," ");
+
+          this.calculations.push(this.currentCalc);
+
+          this.input = []
+          break;
+        default:
+          this.input.push(key);
+      }
+    }
+  },
+  computed: {
+    currentCalc(){
+      return this.input.join("");
+    },
+    getFullCalculation(){
+      let answer = this.answer;
+      this.input.forEach(function(item, index){
+        if(item === 'ANS'){
+            this[index] = answer;
+        }
+      }, this.input)
+      console.log(this.input);
+      return this.input.join("");
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 10px;
+  font-size: 1.6em;
+}
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
